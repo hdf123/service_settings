dde062943e60ce7be5b64174f9e5c5e7b6a6b7b1
hdf123 <15137102387@163.com>
2018-09-08 23:38:50 +0800

    00

---
+++ 优选搭配/server.js
@@ -0,0 +1,35 @@
+//引进模块
+var http=require("http");
+var fs=require("fs");
+var ur1=require("url");
+//var qu1=require("querystring");
+http.createServer(function(req,res){
+//	设置跨域
+	res.setHeader("Access-Control-Allow-Origin","*");
+//	禁止图标
+//	if(req.url=="/favicon.icon"){
+//		res.end();
+//		return;
+//	}
+	switch(req.url){
+		case "/json1" :
+		fun("json1.json",req,res);
+		break;
+		case "/json2" :
+		fun("json2.json",req,res)
+		break;
+		default://以上全不满足
+		res.end("加载失败");
+		break;
+	}
+}).listen(6688)
+console.log("启动成功")
+//封装函数
+function fun(tt,req,res){
+	fs.readFile(tt,"utf8",function(err,data){
+		if(!err){
+			res.end(data)
+			console.log(data)
+		}
+	})
+}
